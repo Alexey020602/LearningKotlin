94b36f400e7d807309b25b2560089c59d619a8a3
Алексей Федоров <Fedorov.Alexey@live.ru>
2022-01-02 11:45:27 +0300

    Добавил в класс

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,7 +1,22 @@
 fun main(args: Array<String>) {
-    println("Hello, ${args[0]}!")
+    val pers=Person("qqqq")
+    pers.printName()
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     //println("Program arguments: ${args.joinToString()}")
 }
+
+class Person(_name: String){
+    val name: String
+    init{
+        name = _name
+    }
+    fun printName(){
+        println("Hello, ${name}!")
+    }
+}
+//class Person{
+    //var name: String = "Undefined"
+    //var age: Int = 18
+//}
